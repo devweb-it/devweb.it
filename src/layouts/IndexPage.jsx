93621cda2f4b3942774e8491ec2dfff20754b653
Alessandro Sebastiani <sebbalex@gmail.com>
2021-09-19 16:29:15 +0200

    feat: footer impl

--- src/layouts/IndexPage.jsx
+++ src/layouts/IndexPage.jsx
@@ -4,8 +4,8 @@ import * as React from 'react';
 export const IndexPage = () => {
   const intl = useIntl();
   return (
-    <>
+    <div className="container" style={{ minHeight: '300px' }}>
       <h1>{intl.formatMessage({ id: 'pages.homepage.title' })}</h1>
-    </>
+    </div>
   );
 };
