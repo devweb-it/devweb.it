7c921d7013ad2f16edafb9e4a9981601f1b49f89
Alessandro Sebastiani <sebbalex@gmail.com>
2021-10-19 19:41:25 +0200

    chore: switch to react-bootstrap components for header

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,109 +1,75 @@
 import * as React from 'react';
 import { useIntl } from 'gatsby-plugin-intl';
-import { Dropdown } from 'react-bootstrap';
-// import { Nav } from 'react-bootstrap';
+import { Container, Dropdown, Nav, Navbar } from 'react-bootstrap';
 import PropTypes from 'prop-types';
-import { Link } from 'gatsby';
 
 export const Header = ({ languages }) => {
   const intl = useIntl();
   return (
     <>
-      {/* <Nav
-      activeKey="/home"
-      className="py-2 bg-light border-bottom"
-      onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
-    >
-      <Nav.Item>
-        <Nav.Link href="/">Home</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey="#features">Features</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey="#pricing">Pricing</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey="#about">About</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey="#faqs" disabled>
-          FAQs
-        </Nav.Link>
-      </Nav.Item>
-    </Nav>
-    <Nav
-      activeKey="/home"
-      className="py-2 bg-light border-bottom"
-      onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
-    >
-      <Nav.Item>
-        <Nav.Link eventKey="#login">Login</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey="#signup">Signup</Nav.Link>
-      </Nav.Item>
-    </Nav> */}
-      <nav className="py-2 bg-light border-bottom">
-        <div className="container d-flex flex-wrap">
-          <ul className="nav me-auto">
-            <li className="nav-item">
-              <Link
-                to={intl.formatMessage({ id: 'common.internalLinks.home.linkTo' })}
-                className="nav-link link-dark px-2 active"
+      <Navbar collapseOnSelect expand="lg" bg="ligth" variant="light" sticky="top">
+        <Container>
+          <Navbar.Brand href="#home">Boilerplate</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link
+                href={intl.formatMessage({
+                  id: 'common.internalLinks.home.linkTo',
+                })}
                 aria-current="page"
               >
                 {intl.formatMessage({ id: 'common.internalLinks.home.label' })}
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                to={intl.formatMessage({ id: 'common.internalLinks.features.linkTo' })}
-                className="nav-link link-dark px-2"
+              </Nav.Link>
+              <Nav.Link
+                href={intl.formatMessage({
+                  id: 'common.internalLinks.features.linkTo',
+                })}
               >
-                {intl.formatMessage({ id: 'common.internalLinks.features.label' })}
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                to={intl.formatMessage({ id: 'common.internalLinks.about.linkTo' })}
-                className="nav-link link-dark px-2"
+                {intl.formatMessage({
+                  id: 'common.internalLinks.features.label',
+                })}
+              </Nav.Link>
+              <Nav.Link
+                href={intl.formatMessage({
+                  id: 'common.internalLinks.about.linkTo',
+                })}
               >
                 {intl.formatMessage({ id: 'common.internalLinks.about.label' })}
-              </Link>
-            </li>
-          </ul>
-          <ul className="nav">
-            <li className="nav-item">
-              <Link
-                to={intl.formatMessage({ id: 'common.internalLinks.login.linkTo' })}
-                className="nav-link link-dark px-2"
+              </Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link
+                href={intl.formatMessage({
+                  id: 'common.internalLinks.login.linkTo',
+                })}
               >
                 {intl.formatMessage({ id: 'common.internalLinks.login.label' })}
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                to={intl.formatMessage({ id: 'common.internalLinks.signup.linkTo' })}
-                className="nav-link link-dark px-2"
+              </Nav.Link>
+              <Nav.Link
+                href={intl.formatMessage({
+                  id: 'common.internalLinks.signup.linkTo',
+                })}
               >
-                {intl.formatMessage({ id: 'common.internalLinks.signup.label' })}
-              </Link>
-            </li>
-          </ul>
-          <Dropdown>
-            <Dropdown.Toggle id="dropdown-basic">{intl.locale.toUpperCase()}</Dropdown.Toggle>
+                {intl.formatMessage({
+                  id: 'common.internalLinks.signup.label',
+                })}
+              </Nav.Link>
+              <Dropdown>
+                <Dropdown.Toggle id="dropdown-basic">{intl.locale.toUpperCase()}</Dropdown.Toggle>
 
-            <Dropdown.Menu>
-              {languages.map((x) => (
-                <Dropdown.Item key={x} href={`/${x}`}>
-                  {x.toUpperCase()}
-                </Dropdown.Item>
-              ))}
-            </Dropdown.Menu>
-          </Dropdown>
-        </div>
-      </nav>
+                <Dropdown.Menu>
+                  {languages.map((x) => (
+                    <Dropdown.Item key={x} href={`/${x}`}>
+                      {x.toUpperCase()}
+                    </Dropdown.Item>
+                  ))}
+                </Dropdown.Menu>
+              </Dropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
       <header className="py-3 mb-4 border-bottom">
         <div className="container d-flex flex-wrap justify-content-center">
           <a href="/" className="d-flex align-items-center mb-3 mb-lg-0 me-lg-auto text-dark text-decoration-none">
